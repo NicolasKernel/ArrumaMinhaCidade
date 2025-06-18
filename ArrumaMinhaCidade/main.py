9dcf7af58b60fc1186bab0df13ce676f4d106db2
NicolasKernel <nicolas.c.santosx@gmail.com>
2025-06-18 09:33:16 -0300

    Update main.py

--- ArrumaMinhaCidade/main.py
+++ ArrumaMinhaCidade/main.py
@@ -1,13 +1,16 @@
 from kivy.app import App
 from kivy.uix.screenmanager import ScreenManager
 from screens.login import LoginScreen
+from screens.cadastro import CadastroScreen
 from screens.perfil import PerfilScreen
 from screens.landing import LandingScreen
 from screens.blog import BlogScreen
 from screens.service_update import ServiceUpdateScreen
 from controllers.navigation import NavigationController
 from kivy.core.window import Window
+from kivy.config import Config
 
+Config.set('input', 'mouse', 'mouse,multitouch_on_demand')
 Window.clearcolor = (1, 1, 1, 1)
 Window.maximize()
 
@@ -17,12 +20,12 @@ class ScreenManagement(ScreenManager):
 class MainApp(App):
     def build(self):
         sm = ScreenManagement()
-        nav = NavigationController(sm)
         sm.add_widget(LoginScreen(name='login'))
+        sm.add_widget(CadastroScreen(name='cadastro'))
         sm.add_widget(PerfilScreen(name='perfil'))
         sm.add_widget(LandingScreen(name='landing'))
-        sm.add_widget(BlogScreen(name='blog'))
         sm.add_widget(ServiceUpdateScreen(name='service_update'))
+        sm.add_widget(BlogScreen(name='blog'))
         return sm
 
 if __name__ == '__main__':
