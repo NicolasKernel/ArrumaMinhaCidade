1921a6ed02db9f77adb0c0b8e31e0f25e7441aac
NicolasKernel <nicolas.c.santosx@gmail.com>
2025-06-23 11:08:07 -0300

    Update user.py

--- models/user.py
+++ models/user.py
@@ -1,7 +1,18 @@
 class User:
-    def __init__(self, username, email):
+    def __init__(self, username, email, telefone, cpf, cep, bairro, senha, is_admin=False):
         self.username = username
         self.email = email
+        self.telefone = telefone
+        self.cpf = cpf
+        self.cep = cep
+        self.bairro = bairro
+        self.senha = senha
+        self.is_admin = is_admin
 
     def get_info(self):
-        return f"Usuário: {self.username}, Email: {self.email}"
+        return (
+            f"Usuário: {self.username}, Email: {self.email}, "
+            f"Telefone: {self.telefone}, CPF: {self.cpf}, "
+            f"CEP: {self.cep}, Bairro: {self.bairro}, "
+            f"Admin: {self.is_admin}"
+        )
