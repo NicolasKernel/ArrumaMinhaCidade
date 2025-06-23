591e8fd97b10b6ab628aa5ed025b497db21adcf0
NicolasKernel <nicolas.c.santosx@gmail.com>
2025-06-23 09:34:46 -0300

    Delete ArrumaMinhaCidade directory

--- ArrumaMinhaCidade/models/user.py
+++
@@ -1,7 +0,0 @@
-class User:
-    def __init__(self, username, email):
-        self.username = username
-        self.email = email
-
-    def get_info(self):
-        return f"Usuário: {self.username}, Email: {self.email}"
--- ArrumaMinhaCidade/utils/validation.py
+++
@@ -1,4 +0,0 @@
-def validate_email(email):
-    if '@' in email and '.' in email:
-        return True
-    return False
