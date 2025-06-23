2a6c28e8541ba21828471666359dc83761d9ab7d
NicolasKernel <nicolas.c.santosx@gmail.com>
2025-06-23 11:07:44 -0300

    Update login.py
    Atualizado para checar a existencia do usuario no json

--- screens/login.py
+++ screens/login.py
@@ -8,6 +8,7 @@ from kivy.uix.widget import Widget
 from kivy.graphics import Color, Rectangle
 from kivy.uix.popup import Popup
 import os
+import json
 
 class LoginScreen(Screen):
     def __init__(self, **kwargs):
@@ -85,10 +86,7 @@ class LoginScreen(Screen):
         self.error_label = Label(text='', font_size=13, color=(1, 0, 0, 1))
         main_layout.add_widget(self.error_label)
 
-        self.usuarios = {
-            'ABC': 'ABC',
-            '111111111': '456'
-        }
+        self.users_json = "usuarios.json"
 
     def _update_bg_rect(self, instance, value):
         self.bg_rect.pos = instance.pos
@@ -97,7 +95,21 @@ class LoginScreen(Screen):
     def login_action(self, instance):
         cpf = self.cpf_input.text
         senha = self.senha_input.text
-        if cpf in self.usuarios and self.usuarios[cpf] == senha:
+
+        # Checa no JSON de usuários
+        try:
+            with open(self.users_json, "r", encoding="utf-8") as f:
+                users_data = json.load(f)
+        except (FileNotFoundError, json.JSONDecodeError):
+            users_data = []
+
+        usuario_encontrado = False
+        for user in users_data:
+            if user.get("cpf") == cpf and user.get("senha") == senha:
+                usuario_encontrado = True
+                break
+
+        if usuario_encontrado:
             print(f"Usuário {cpf} logado com sucesso!")
             if self.manager and 'landing' in self.manager.screen_names:
                 self.manager.current = 'landing'
