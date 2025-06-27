d02609f5bf3aba6df9b6252042e1b89f016a22d9
NicolasKernel <nicolas.c.santosx@gmail.com>
2025-06-27 11:07:36 -0300

    Delete controllers directory

--- controllers/navigation.py
+++
@@ -1,6 +0,0 @@
-class NavigationController:
-    def __init__(self, screen_manager):
-        self.sm = screen_manager
-
-    def navigate_to(self, screen_name):
-        self.sm.current = screen_name
